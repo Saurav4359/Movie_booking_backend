2221c78919510ce0d2c5a8412caa30125030c456
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-11-23 04:20:38 +0530

    link/navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Route, Routes,Link,useNavigate } from "react-router-dom";
+import "./App.css";
 
+// install react-router-dom.
 function App() {
-  
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/neet/online/class-11" element={<Class11Program/>}/>  
-      <Route path="/neet/online/class-12" element={<Class12Program/>}/>
-      <Route path="/" element={<Landing/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+      <Link  to="/">Allen</Link> 
+      <Link  to="/neet/online/class-11">Class 11</Link> 
+      <Link  to="/neet/online/class-12">Class 12</Link> 
+        <Routes>
+          <Route path="/neet/online/class-11" element={<Class11Program />} />
+          <Route path="/neet/online/class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </ BrowserRouter>
     </>
-  )
+  );
 }
 
 function Class11Program() {
-  return <div>
-    NEET programs for class 11th.
-  </div>
+  return <div>NEET programs for class 11th.</div>;
 }
 function Class12Program() {
+  const navigate=useNavigate();
+  function redirectUser() {
+    navigate("/");
+  }
   return <div>
     NEET programs for class 12th.
-  </div>
+    <button onClick={redirectUser}>Go back to landing page. </button></div>;
 }
-  function Landing() {
-    return <>
-    Welcome to Allen !.
-    </>
-  }
+function Landing() {
+  return <>Welcome to Allen !.</>;
+}
 
-export default App
+export default App;
